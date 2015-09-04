a78f3b9c20ccdaeda340450f15920fb9450ecf89
Pierre Zurek <pierre.zurek@parrot.com>
2015-09-04 10:46:08 +0200

    [DEV] Add libressl-2.2.1

--- libressl/atom.mk
+++ libressl/atom.mk
@@ -4,53 +4,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := curl
-LOCAL_DESCRIPTION := cURL
+LOCAL_MODULE := libressl
+LOCAL_DESCRIPTION := LibreSSL
 LOCAL_CATEGORY_PATH := libs
 
-LOCAL_AUTOTOOLS_VERSION := 7.36.0
-LOCAL_AUTOTOOLS_ARCHIVE := curl-$(LOCAL_AUTOTOOLS_VERSION).tar.bz2
-LOCAL_AUTOTOOLS_SUBDIR := curl-$(LOCAL_AUTOTOOLS_VERSION)
+LOCAL_AUTOTOOLS_VERSION := 2.2.1
+LOCAL_AUTOTOOLS_ARCHIVE := libressl-$(LOCAL_AUTOTOOLS_VERSION).tar.gz
+LOCAL_AUTOTOOLS_SUBDIR := libressl-$(LOCAL_AUTOTOOLS_VERSION)
 LOCAL_AUTOTOOLS_PATCHES := \
-	0001-ARSDK-Allow-mixed-32-64bits-builds.patch \
-	0002-ARSDK-Allow-iOS-x86_64-builds-for-simulators.patch \
-	avoid-version.patch
-
-LOCAL_EXPORT_LDLIBS := -lcurl
-LOCAL_LIBRARIES := libressl
+	libressl-2.2.1.patch
 
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
-	--disable-static \
-	--enable-shared \
-	--disable-ares \
-	--disable-file \
-	--disable-ldap \
-	--disable-ldaps \
-	--disable-rtsp \
-	--disable-proxy \
-	--disable-dict \
-	--disable-telnet \
-	--disable-tftp \
-	--disable-pop3 \
-	--disable-imap \
-	--disable-smtp \
-	--disable-gopher \
-	--disable-manual \
-	--disable-verbose \
-	--disable-sspi \
-	--disable-ntlm-wb \
-	--disable-tls-srp \
-	--without-winssl \
-	--without-gnutls \
-	--without-polarssl \
-	--without-cyassl \
-	--without-nss \
-	--without-axtls \
-	--without-ca-bundle \
-	--without-libmetalink \
-	--without-libssh2 \
-	--without-librtmp \
-	--without-winidn \
+	--enable-static \
+	--disable-shared \
 	LIBS=" -llog -lz" INSTALL="/usr/bin/install -C"
 
 ifeq ($(ARSDK_BUILD_ARCH), armeabi)
@@ -78,7 +44,7 @@ endif
 # User define command to be launch before configure step.
 # Generates files used by configure
 define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
-	$(Q) cd $(PRIVATE_SRC_DIR) && rm src/tool_hugehelp.c && autoreconf -fiv
+	$(Q) cd $(PRIVATE_SRC_DIR) && autoreconf -fiv
 endef
 
 # Don't redefine alloc functions
