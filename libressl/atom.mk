71acd724e82a05a1ecbbb612d2420c3ef7abcd3a
Djavan Bertrand <djavan.bertrand@parrot.com>
2017-10-23 17:34:10 +0200

    [DEV] Add support for Android-ndk unified headers
    This commit is a copy of the one made by
    Nicolas Brulez on the json standalone repo.
    
    Change-Id: I426b91203412996af9bd8aeec95936b2028834ba
    Reviewed-on: https://gerrit-doc.parrot.biz/80765
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- libressl/atom.mk
+++ libressl/atom.mk
@@ -1,5 +1,3 @@
-ifdef ARSDK_BUILD_FOR_APP
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -16,11 +14,16 @@ LOCAL_AUTOTOOLS_PATCHES := \
 	0001-libressl-2.2.1-maintainer-mode.patch \
 	0002-libressl-2.2.1-android-compilation.patch \
 	0003-libressl-2.2.1-ios-compilation.patch \
-	0004-libressl-2.2.1-armv5.patch
+	0004-libressl-2.2.1-armv5.patch \
+	0005-libressl-2.2.1-android_unified_headers.patch
 
+ifneq ("$(TARGET_OS)-$(TARGET_OS_FLAVOUR)","linux-native")
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
 	--enable-static \
 	--disable-shared
+endif
+
+LOCAL_EXPORT_LDLIBS := -lssl -lcrypto
 
 # Don't redefine alloc functions
 LOCAL_AUTOTOOLS_CONFIGURE_ENV := \
@@ -28,5 +31,3 @@ LOCAL_AUTOTOOLS_CONFIGURE_ENV := \
 	ac_cv_func_realloc_0_nonnull=yes \
 
 include $(BUILD_AUTOTOOLS)
-
-endif
